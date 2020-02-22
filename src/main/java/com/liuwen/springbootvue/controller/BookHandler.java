81d2b8a0f661b84e84c944cbbf4df71a31ced9d7
liuwen766 <780442619@qq.com>
2020-02-22 16:22:34 +0800

    图书管理后端应用：5.后端应用提供数据

---
+++ src/main/java/com/liuwen/springbootvue/controller/BookHandler.java
@@ -0,0 +1,59 @@
+package com.liuwen.springbootvue.controller;
+
+
+import com.liuwen.springbootvue.entity.Book;
+import com.liuwen.springbootvue.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+public class BookHandler {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/findAll")
+    public List<Book> findAll(){
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/findAll/{page}/{size}")
+    public Page<Book> findAll(@PathVariable("page") Integer page, @PathVariable("size") Integer size){
+        PageRequest request = PageRequest.of(page,size);      //分页
+        return bookRepository.findAll(request);
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestBody Book book){
+        Book result = bookRepository.save(book);
+        if(result != null){
+            return "success";
+        }else{
+            return "error";
+        }
+    }
+
+    @GetMapping("/findById/{id}")
+    public Book findById(@PathVariable("id") Integer id){
+        return bookRepository.findById(id).get();
+    }
+
+    @PutMapping("/update")
+    public String update(@RequestBody Book book){
+        Book result = bookRepository.save(book);
+        if(result != null){
+            return "success";
+        }else{
+            return "error";
+        }
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public void deleteById(@PathVariable("id") Integer id){
+        bookRepository.deleteById(id);
+    }
+}
